01cf093527dfd803ff7e5fc0b91dc67e157833bf
cargibamba <magicgibsonbeat@gmail.com>
2023-11-28 05:30:03 +0700

    cypress baru

--- cypress/e2e/3-web/magento.softwaretestingboard/login.cy.js
+++ cypress/e2e/3-web/magento.softwaretestingboard/login.cy.js
@@ -1,7 +1,9 @@
+const userData = require ('../../../fixtures/userData.json')
+
 describe('Verify magento functionality', () => {
   it('Failed Login - Locked User', () => {
     cy.visit('https://magento.softwaretestingboard.com/customer/account/login')
-    cy.get('#email').type('agicgibsonbeat@gmail.com')
+    cy.get('#email').type(userData.invalid_email)
     cy.get('.login-container > .block-customer-login > .block-content > #login-form > .fieldset > .password > .control > #pass').type('MagicGibsonBeat123')
     cy.get('.login-container > .block-customer-login > .block-content > #login-form > .fieldset > .actions-toolbar > div.primary > #send2 > span').click()
     cy.get('.message-error > div').should('contain', 'The account sign-in was incorrect or your account is disabled temporarily. Please wait and try again later.')
