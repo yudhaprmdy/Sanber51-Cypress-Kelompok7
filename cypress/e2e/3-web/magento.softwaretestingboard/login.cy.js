19f03e1d9e6233803f9a490ebc5ee5e1cab3d307
cargibamba <magicgibsonbeat@gmail.com>
2023-11-28 08:01:00 +0700

    cypress

--- cypress/e2e/3-web/magento.softwaretestingboard/login.cy.js
+++ cypress/e2e/3-web/magento.softwaretestingboard/login.cy.js
@@ -1,16 +1,18 @@
+import  loginPage from '../../../support/pageObject/loginPage'
 const userData = require ('../../../fixtures/userData.json')
 
 describe('Verify magento functionality', () => {
   it('Failed Login - Locked User', () => {
     cy.visit('https://magento.softwaretestingboard.com/customer/account/login')
-    cy.get('#email').type(userData.invalid_email)
+    cy.get('#email').type(userData.invalid.invalid_email)
     cy.get('.login-container > .block-customer-login > .block-content > #login-form > .fieldset > .password > .control > #pass').type('MagicGibsonBeat123')
     cy.get('.login-container > .block-customer-login > .block-content > #login-form > .fieldset > .actions-toolbar > div.primary > #send2 > span').click()
     cy.get('.message-error > div').should('contain', 'The account sign-in was incorrect or your account is disabled temporarily. Please wait and try again later.')
   })
   it('Succes Login', () => {
     cy.visit('https://magento.softwaretestingboard.com/customer/account/login')
-    cy.get('#email').type('magicgibsonbeat@gmail.com')
+    cy.get(loginPage.email).type(userData.valid.valid_email)
+    //cy.get('#email').type('magicgibsonbeat@gmail.com')
     cy.get('.login-container > .block-customer-login > .block-content > #login-form > .fieldset > .password > .control > #pass').type('MagicGibsonBeat123')
     cy.get('.login-container > .block-customer-login > .block-content > #login-form > .fieldset > .actions-toolbar > div.primary > #send2 > span').click()
     cy.url().should('include','https://magento.softwaretestingboard.com/customer/account/')
---
+++ cypress/support/pageObject/loginPage.js
@@ -0,0 +1,7 @@
+class loginPage {
+    email = '#email' 
+    password = '.login-container > .block-customer-login > .block-content > #login-form > .fieldset > .password > .control > #pass'
+    loginbutton = '.login-container > .block-customer-login > .block-content > #login-form > .fieldset > .actions-toolbar > div.primary > #send2 > span'
+
+}
+    export default new loginPage()
