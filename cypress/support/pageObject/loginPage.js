fa569a5a563f0a96bde3587decd0a1dbd502ded7
cargibamba <magicgibsonbeat@gmail.com>
2023-11-28 08:14:24 +0700

    cypress test

--- cypress/support/pageObject/loginPage.js
+++ cypress/support/pageObject/loginPage.js
@@ -1,4 +1,4 @@
-class loginPage {
+class loginPage  {
     email = '#email' 
     password = '.login-container > .block-customer-login > .block-content > #login-form > .fieldset > .password > .control > #pass'
     loginbutton = '.login-container > .block-customer-login > .block-content > #login-form > .fieldset > .actions-toolbar > div.primary > #send2 > span'
